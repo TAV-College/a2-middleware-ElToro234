6f26c8d110441c268fff3d05eeb497c5055516a2
Alexander Liu <alexanderliuxinlu@gmail.com>
2025-05-13 19:29:49 -0400

    middleare

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 // Base imports
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require('path');
+const logger = require('./middleware/logging');
+const { verifyToken } = require('./middleware/auth');
+
 
 // Router Imports
 const mainRouter = require("./routers/main.js");
 const bookRouter = require("./routers/books.js");
+const authRouter = require('./routers/auth');
 const port = process.env.PORT || 3000;
 
 // Create & Configure App
@@ -12,6 +17,12 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
+
+// Apply routers
+app.use('/', mainRouter);
+app.use('/api', authRouter); // Routes for authentication
+app.use('/api', booksRouter); // Apply token verification to specific routes
+
 
 // Load routers
 app.use("", mainRouter);
